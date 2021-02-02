e236a666e4272adf8d91154eaa79825368eb6554
Andrey Seluykov <trezortop92@gmail.com>
2021-02-02 23:15:08 +0300

    Update social_network.sql

--- SQL_BACKUP/social_network.sql
+++ SQL_BACKUP/social_network.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Янв 26 2021 г., 21:16
+-- Время создания: Фев 02 2021 г., 23:14
 -- Версия сервера: 10.3.22-MariaDB
 -- Версия PHP: 7.1.33
 
@@ -24,6 +24,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `comment` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `posted_at` datetime NOT NULL,
+  `post_id` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `comments`
+--
+
+INSERT INTO `comments` (`id`, `comment`, `user_id`, `posted_at`, `post_id`) VALUES
+(1, 'Hello, this is comment', 2, '2021-02-02 22:25:38', 6),
+(2, 'And another comment!', 2, '2021-02-02 22:25:46', 6),
+(3, 'cool!\r\n', 2, '2021-02-02 22:25:50', 6);
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `followers`
 --
 
@@ -38,7 +61,8 @@ CREATE TABLE `followers` (
 --
 
 INSERT INTO `followers` (`id`, `user_id`, `follower_id`) VALUES
-(1, 2, 1);
+(6, 1, 2),
+(7, 3, 2);
 
 -- --------------------------------------------------------
 
@@ -59,8 +83,17 @@ CREATE TABLE `login_tokens` (
 INSERT INTO `login_tokens` (`id`, `token`, `user_id`) VALUES
 (3, '6906814c91ed8f7cde0c75ce5b0f9e253a34c404', 2),
 (4, '12252015d1f3be317b0ada68362c734fd68a2724', 2),
-(5, '15b99d47cb05635f13dcde01349e69793ee77f07', 1),
-(6, '04e8fa469a9b2399cf243eec4404bdc8f5f57241', 2);
+(6, '04e8fa469a9b2399cf243eec4404bdc8f5f57241', 2),
+(8, '8ed6e17e563f46c247e2371e3913f2c31c2072e7', 1),
+(9, '996e231918c832256e581ccd84f14446ecb2f43b', 2),
+(10, '303b88df0e111b300a729c1bfbc81b86db3c2b22', 2),
+(11, '33dccc4f99da59fffe50c22c71cdaacc88abc080', 1),
+(12, 'f0bde6067afa96ca42c57777c09d0c8c127b7d84', 2),
+(13, '7ea1e527c04223e2624a3adea0139bd479e5a9a7', 1),
+(14, 'ff989fba5f48ac915f3a995dd245678faf28c62c', 2),
+(15, '0799d648735a417aae0155bcac739e064162b72e', 3),
+(16, 'a0c31008833924ea511b3a7fcd70afc756a66518', 2),
+(18, '7057c5342bd65e843c49f25c59ff5d79feab96d4', 2);
 
 -- --------------------------------------------------------
 
@@ -95,9 +128,14 @@ CREATE TABLE `posts` (
 INSERT INTO `posts` (`id`, `body`, `posted_at`, `user_id`, `likes`) VALUES
 (1, 'Hello World', '2021-01-25 19:50:10', 2, 1),
 (2, 'Second Post', '2021-01-25 19:52:34', 2, 1),
-(3, 'Third Post!', '2021-01-25 19:55:56', 2, 1),
-(4, 'Test', '2021-01-26 20:15:46', 2, 1),
-(5, 'Hello <b>World</b>', '2021-01-26 20:16:08', 2, 1);
+(3, 'Third Post!', '2021-01-25 19:55:56', 2, 0),
+(4, 'Test', '2021-01-26 20:15:46', 2, 2),
+(5, 'Hello <b>World</b>', '2021-01-26 20:16:08', 2, 0),
+(6, 'echo $loggedInUserId;', '2021-01-26 21:27:43', 1, 0),
+(7, 'This is a post', '2021-01-26 21:28:01', 1, 0),
+(8, 'Hello guys! This is my first post!', '2021-01-26 21:45:03', 3, 0),
+(9, 'Another test post', '2021-01-26 21:45:40', 3, 0),
+(10, 'NEW TEST POST', '2021-02-01 20:42:18', 2, 1);
 
 -- --------------------------------------------------------
 
@@ -116,11 +154,11 @@ CREATE TABLE `post_likes` (
 --
 
 INSERT INTO `post_likes` (`id`, `post_id`, `user_id`) VALUES
-(12, 3, 2),
 (13, 1, 2),
 (14, 4, 1),
 (15, 2, 1),
-(17, 5, 2);
+(19, 10, 2),
+(23, 4, 2);
 
 -- --------------------------------------------------------
 
@@ -142,11 +180,20 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `username`, `password`, `email`, `verified`) VALUES
 (1, 'Verified', '$2y$10$RAQVJfIYIyoattOl4t/5SeM4YsYVhrGB.PP5hTS.B3TUonH7cZStm', 'trezortop92@gmail.com', 0),
-(2, 'TrezorTop', '$2y$10$1ojYzDjwyvlKQafyM9qIWehVoA6AIOB./opR5gN75PWd1xvxsuSrW', 'trezorsix@mail.ru', 1);
+(2, 'TrezorTop', '$2y$10$1ojYzDjwyvlKQafyM9qIWehVoA6AIOB./opR5gN75PWd1xvxsuSrW', 'trezorsix@mail.ru', 0),
+(3, 'Andrey', '$2y$10$gmDkI2UpC2KFG/VgY47Ts.akeYUsfs9dwlCAn1aorMisFF0XlO4BK', 'trezor6@mail.ru', 0);
 
 --
 -- Индексы сохранённых таблиц
 --
+
+--
+-- Индексы таблицы `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `post_id` (`post_id`);
 
 --
 -- Индексы таблицы `followers`
@@ -195,16 +242,22 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT для таблицы `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT для таблицы `followers`
 --
 ALTER TABLE `followers`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT для таблицы `login_tokens`
 --
 ALTER TABLE `login_tokens`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT для таблицы `password_tokens`
@@ -216,23 +269,30 @@ ALTER TABLE `password_tokens`
 -- AUTO_INCREMENT для таблицы `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT для таблицы `post_likes`
 --
 ALTER TABLE `post_likes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
 --
+
+--
+-- Ограничения внешнего ключа таблицы `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`);
 
 --
 -- Ограничения внешнего ключа таблицы `login_tokens`
