0da3e71b69ede9dfe1ba18fdbce2efe0faf1e66d
Andrey Seluykov <trezortop92@gmail.com>
2021-02-10 19:52:28 +0300

    profile images
    profile images in database are updating now

--- pages/my-account.php
+++ pages/my-account.php
@@ -1,5 +1,14 @@
 <?php
 
+include $_SERVER['DOCUMENT_ROOT'] . '/php/connect.php';
+include $_SERVER['DOCUMENT_ROOT'] . '/classes/Login.php';
+
+if (Login::isLoggedIn($link)) {
+    $userId = Login::isLoggedIn($link);
+} else {
+    die('Not logged in!');
+}
+
 if (isset($_POST['upload-profile-img'])) {
 
     $image = base64_encode(file_get_contents($_FILES['profile-img']['tmp_name']));
@@ -15,7 +24,21 @@ if (isset($_POST['upload-profile-img'])) {
 
     $imgurURL = "https://api.imgur.com/3/image";
 
+    if ($_FILES['profile-img']['size'] > 52428800) {
+        die('Image too big must be 50MB or less!');
+    }
+
     $response = file_get_contents($imgurURL, false, $context);
+
+    $response = json_decode($response);
+
+    echo '<pre>';
+    print_r($response);
+    echo '</pre>';
+
+    $linkFromResponsedData = $response->data->link;
+
+    mysqli_query($link, "UPDATE users SET profileimg = '$linkFromResponsedData' WHERE id = '$userId'");
 }
 
 ?>
