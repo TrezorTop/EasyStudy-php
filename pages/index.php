facdd70f74cffe8f84514b4274e99c114ea05d88
Andrey Seluykov <trezortop92@gmail.com>
2021-02-25 19:38:14 +0300

    users and posts search

--- pages/index.php
+++ pages/index.php
@@ -20,6 +20,46 @@ if (isset($_POST['comment'])) {
     Comment::postComment($_POST['comment-body'], $_GET['postId'], $userId);
 }
 
+if (isset($_POST['searchbox'])) {
+    $toSearch = explode(" ", $_POST['searchbox']);
+    if (count($toSearch) == 1) {
+        $toSearch = str_split($toSearch[0], 2);
+    }
+
+    $whereClause = "";
+    $paramsArray = array(':username' => '%' . $_POST['searchbox'] . '%');
+    for ($i = 0; $i < count($toSearch); $i++) {
+        $whereClause .= " OR username LIKE :u$i ";
+        $paramsArray[":u$i"] = $toSearch[$i];
+    }
+
+    $users = DB::query("SELECT users.username FROM users WHERE users.username LIKE :username $whereClause", $paramsArray);
+    print_r($users);
+
+    $whereClause = "";
+    $paramsArray = array(':body' => '%' . $_POST['searchbox'] . '%');
+    for ($i = 0; $i < count($toSearch); $i++) {
+        if ($i % 2) {
+            $whereClause .= " OR body LIKE :p$i ";
+            $paramsArray[":p$i"] = $toSearch[$i];
+        }
+    }
+    $posts = DB::query("SELECT posts.body FROM posts WHERE posts.body LIKE :body $whereClause", $paramsArray);
+    echo '<pre>';
+    print_r($posts);
+    echo '</pre>';
+}
+
+
+?>
+
+    <form action="index.php" method="post">
+        <input type="text" name="searchbox" value="">
+        <input type="submit" name="search" value="Search">
+    </form>
+
+<?php
+
 $followingPosts = DB::query('SELECT posts.id, posts.body, posts.likes, users.`username` FROM users, posts, followers
 WHERE posts.user_id = followers.user_id
 AND users.id = posts.user_id
